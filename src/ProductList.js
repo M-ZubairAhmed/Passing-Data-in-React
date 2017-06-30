ccb05434d414bbb074d789f3cb3da4b38397afb8
Mohammed Zubair Ahmed <ahmedzubair216@gmail.com>
2017-06-30 23:49:15 +0530

    new form component for inputing price

---
+++ src/ProductList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Product from './Product';
+import Total from './Total';
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productData: [
+        { model: 'Dell', cost: 20 },
+        { model: 'Apple', cost: 100 },
+        { model: 'HP', cost: 50 }
+      ],
+      total: 0,
+      price: null
+    };
+  }
+
+  calculatingTotal = price => {
+    this.setState({
+      total: price + this.state.total
+    });
+  };
+
+  render() {
+    const products = this.state.productData.map(product =>
+      <Product
+        model={product.model}
+        cost={product.cost}
+        calculatingTotal={this.calculatingTotal}
+      />
+    );
+    return (
+      <div>
+        {products}
+        <Total total={this.state.total} />
+      </div>
+    );
+  }
+}
+
+export default ProductList;
